89e8770ca0464299274fbf78d6818d1986edd499
Yaroslav <yarik.kiknul@gmail.com>
2024-06-12 12:05:09 +0300

    Reproject-full

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const footerContainer = document.querySelector('.modal-overlay');
-    var btn =  document.querySelector('.solutions-button');
-    var btnFooter =  document.querySelector('.footer-subscribe-button');
-    var btnCloseModal = document.querySelector('.form-exit-decor');
-    btn.addEventListener('click',function(e){
-        e.preventDefault();
-        footerContainer.classList.toggle('active');
-    });
-    btnFooter.addEventListener('click',function(e){
-        e.preventDefault();
-        footerContainer.classList.toggle('active');
-    });
-    btnCloseModal.addEventListener('click',function(e){
-        e.preventDefault();
-        footerContainer.classList.toggle('active');
-    });
-}); 
+document.addEventListener('DOMContentLoaded', function () {
+	const modalOverlay = document.getElementById('modalOverlay')
+	const mobileMenu = document.getElementById('mobileMenu')
+	const burgerButton = document.getElementById('burgerButton')
+	const closeMenuButton = document.getElementById('closeMenuButton')
+	const btn = document.querySelector('.solutions-button')
+	const btnFooter = document.querySelector('.footer-subscribe-button')
+	const btnCloseModal = document.getElementById('closeModal')
+
+	burgerButton.addEventListener('click', function () {
+		mobileMenu.classList.toggle('is-open')
+	})
+
+	closeMenuButton.addEventListener('click', function () {
+		mobileMenu.classList.remove('is-open')
+	})
+
+	btn.addEventListener('click', function (e) {
+		e.preventDefault()
+		modalOverlay.classList.add('is-open')
+	})
+
+	btnFooter.addEventListener('click', function (e) {
+		e.preventDefault()
+		modalOverlay.classList.add('is-open')
+	})
+
+	btnCloseModal.addEventListener('click', function () {
+		modalOverlay.classList.remove('is-open')
+	})
+})
